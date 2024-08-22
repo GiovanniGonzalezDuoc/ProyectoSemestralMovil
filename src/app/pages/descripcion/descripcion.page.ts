5e2b73b976a5c7d17e06611d3ea8c30fe0f8e31e
Giovanni <gi.gonzalezm@duocuc.cl>
2024-08-22 18:46:12 -0400

    Mejoras Description

--- src/app/pages/descripcion/descripcion.page.ts
+++ src/app/pages/descripcion/descripcion.page.ts
@@ -16,8 +16,7 @@ export class DescripcionPage implements OnInit {
       titulo:"Aguante Brasil",
       mensaje:"Cristiano Ronaldo Mejor Campeon Do Fochibole",
       like:25,
-      comentarios:10,
-      mensajescomentarios:"Cristiano Ronaldo Is Better Of Messi",
+      comentarios:10
    },
     {
       id:2,
@@ -27,8 +26,7 @@ export class DescripcionPage implements OnInit {
       titulo:"Aguante Brasil",
       mensaje:"Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual antes de insertar el texto final. Aunque no posee actualmente fuentes para justificar sus hipótesis, el profesor de filología clásica Richard McClintock asegura que su uso se remonta a los impresores de comienzos del siglo XVI. Su uso en algunos editores de texto muy conocidos en la actualidad ha dado al texto lorem ipsum nueva popularidad.",
       like:25,
-      comentarios:10,
-      mensajescomentarios:"Cristiano Ronaldo Is Better Of Messi",
+      comentarios:10
     },
     {
       id:3,
@@ -38,8 +36,7 @@ export class DescripcionPage implements OnInit {
       titulo:"Aguante Brasil",
       mensaje:"Cristiano Ronaldo Mejor Campeon Do Fochibole",
       like:25,
-      comentarios:10,
-      mensajescomentarios:"Cristiano Ronaldo Is Better Of Messi",
+      comentarios:10
     },
     {
       id:4,
@@ -49,10 +46,40 @@ export class DescripcionPage implements OnInit {
       titulo:"Aguante Brasil",
       mensaje:"Cristiano Ronaldo Mejor Campeon Do Fochibole",
       like:25,
-      comentarios:10,
-      mensajescomentarios:"Cristiano Ronaldo Is Better Of Messi",
+      comentarios:10
     },
   ]
+  comentarios:any=[
+    {
+      id:1,
+      foto:"assets/icon/favicon.png",
+      nombreperfil:"Juan El Ganador",
+      horas:25,
+      mensajecomentario:"Cristiano Mejor Domundo"
+    },
+    {
+      id:2,
+      foto:"assets/icon/favicon.png",
+      nombreperfil:"Juan El Ganador",
+      horas:25,
+      mensajecomentario:"Cristiano Mejor Domundo"
+    },
+    {
+      id:3,
+      foto:"assets/icon/favicon.png",
+      nombreperfil:"Juan El Ganador",
+      horas:25,
+      mensajecomentario:"Cristiano Mejor Domundo"
+    },
+    {
+      id:4,
+      foto:"assets/icon/favicon.png",
+      nombreperfil:"Juan El Ganador",
+      horas:25,
+      mensajecomentario:"Cristiano Mejor Domundo"
+    },
+  ]
+
 
   nuevoComentario: string = '';
 
